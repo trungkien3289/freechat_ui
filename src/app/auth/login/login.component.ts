11c37631cc98196be313ef1253454eb05762ed65
karl <trungkien3289@gmail.com>
2025-01-24 23:56:56 +0700

    migrate ui

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent {
           this.userService.login(this.loginForm.value)
         );
         localStorage.setItem('token', response.token);
-        if(response.isExpired != null && response.isExpired == true) {
+        if (response.isExpired != null && response.isExpired == true) {
           this.router.navigate(['/auth/user-expired']);
         }
         this.router.navigate(['/chatbox']);
