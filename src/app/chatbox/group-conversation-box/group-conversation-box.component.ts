2ffe2dc22eda24262bb57900edc1b396c892632a
karl <trungkien3289@gmail.com>
2025-01-24 20:19:30 +0700

    disable limit send message rule

--- src/app/chatbox/group-conversation-box/group-conversation-box.component.ts
+++ src/app/chatbox/group-conversation-box/group-conversation-box.component.ts
@@ -237,18 +237,18 @@ export class GroupConversationBoxComponent
   debouncedSubmit = debounce(async () => {
     if (this.isRecording) return;
 
-    if (
-      !this._ChatService.canSendMessage(
-        this.contactGroup.currentPhoneNumber.phoneNumber
-      )
-    ) {
-      this._NotificationService.warning(
-        `Cannot send messages in next ${this._ChatService.getWaitToSendSeconds(
-          this.contactGroup.currentPhoneNumber.phoneNumber
-        )} second(s)`
-      );
-      return;
-    }
+    // if (
+    //   !this._ChatService.canSendMessage(
+    //     this.contactGroup.currentPhoneNumber.phoneNumber
+    //   )
+    // ) {
+    //   this._NotificationService.warning(
+    //     `Cannot send messages in next ${this._ChatService.getWaitToSendSeconds(
+    //       this.contactGroup.currentPhoneNumber.phoneNumber
+    //     )} second(s)`
+    //   );
+    //   return;
+    // }
 
     this.isLoading = true;
     // If have images upload
