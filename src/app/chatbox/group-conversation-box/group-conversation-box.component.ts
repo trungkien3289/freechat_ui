eb2c4f8a7f22b82a66923d51269092ab436a06c2
karl <trungkien3289@gmail.com>
2025-02-04 21:07:05 +0700

    update

--- src/app/chatbox/group-conversation-box/group-conversation-box.component.ts
+++ src/app/chatbox/group-conversation-box/group-conversation-box.component.ts
@@ -292,18 +292,18 @@ export class GroupConversationBoxComponent
   debouncedSubmit = debounce(async () => {
     if (this.isRecording) return;
 
-    if (
-      !this._ChatService.canSendMessage(
-        this.contactGroup.currentPhoneNumber.phoneNumber
-      )
-    ) {
-      this._NotificationService.warning(
-        `Cannot send messages in next ${this._ChatService.getWaitToSendMins(
-          this.contactGroup.currentPhoneNumber.phoneNumber
-        )} min(s)`
-      );
-      return;
-    }
+    // if (
+    //   !this._ChatService.canSendMessage(
+    //     this.contactGroup.currentPhoneNumber.phoneNumber
+    //   )
+    // ) {
+    //   this._NotificationService.warning(
+    //     `Cannot send messages in next ${this._ChatService.getWaitToSendMins(
+    //       this.contactGroup.currentPhoneNumber.phoneNumber
+    //     )} min(s)`
+    //   );
+    //   return;
+    // }
 
     this.isLoading = true;
     // If have images upload
